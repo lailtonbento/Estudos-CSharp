132fc49c94f3febdedc4a3cea28a5eb84e7c43c7
Lailton Bento <lailton.bentto@gmail.com>
2022-12-19 15:47:03 -0300

    update files and directory

---
+++ FilesDirectory/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using System.Collections.Generic;
+
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        IEnumerable<string> FindFiles(string folderName)
+        {
+            List<string> salesFiles = new List<string>();
+
+            var founderFiles = Directory.EnumerateFiles(folderName, "*", SearchOption.AllDirectories);
+            foreach (var file in founderFiles)
+            {
+                if (file.EndsWith("sales.json"))
+                    salesFiles.Add(file);
+            }
+
+            return salesFiles;
+        }
+
+        var salesFiles = FindFiles("stores");
+        foreach (var file in salesFiles)
+        {
+            Console.WriteLine(file);
+        }
+        Console.WriteLine("--------------------");
+        // Search for special folder
+        string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        Console.WriteLine(docPath);
+
+        // Class Path é uma classe bem completa para trabalhar com caminhos relativos
+        string fileName = $"stores{Path.DirectorySeparatorChar}201{Path.DirectorySeparatorChar}sales{Path.DirectorySeparatorChar}sales.json";
+
+        FileInfo info = new FileInfo(fileName);
+
+        Console.WriteLine($"Full Name: {info.FullName}{Environment.NewLine}Directory: {info.Directory}{Environment.NewLine}Extension: {info.Extension}{Environment.NewLine}Create Date: {info.CreationTime}");
+    }
+
+}
