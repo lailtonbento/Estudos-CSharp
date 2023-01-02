1655e7281dc725c7035791fe3c0dac0878200e42
Lailton Bento <lailton.bentto@gmail.com>
2023-01-02 16:38:46 -0300

    modulo 18 parte 2

---
+++ Modulo18/SistemaVendas/SistemaVendas/Data/SistemaVendasContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SistemaVendas.Model;
+
+namespace SistemaVendas.Data
+{
+    public class SistemaVendasContext : DbContext
+    {
+        public SistemaVendasContext (DbContextOptions<SistemaVendasContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SistemaVendas.Model.Department> Department { get; set; } = default!;
+    }
+}
---
+++ Modulo18/SistemaVendas/SistemaVendas/Model/Department.cs
@@ -0,0 +1,13 @@
+﻿namespace SistemaVendas.Model
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+
+        public Department()
+        {
+        }
+    }
+}
