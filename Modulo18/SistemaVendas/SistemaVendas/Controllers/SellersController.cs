af6bb233f8f0778a7d3942d4fb327546d62b63aa
Lailton Bento <lailton.bentto@gmail.com>
2023-01-04 15:09:26 -0300

    update2 modulo 18

--- Modulo18/SistemaVendas/SistemaVendas/Controllers/SellersController.cs
+++ Modulo18/SistemaVendas/SistemaVendas/Controllers/SellersController.cs
@@ -32,6 +32,12 @@ namespace SistemaVendas.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Seller seller)
         {
+            if (!ModelState.IsValid)
+            {
+                var departments = _departmentService.FindAll();
+                SellerFormViewModel viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+                return View(viewModel);
+            }
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
@@ -89,6 +95,12 @@ namespace SistemaVendas.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Edit(int id, Seller seller)
         {
+            if (!ModelState.IsValid)
+            {
+                var departments = _departmentService.FindAll();
+                SellerFormViewModel viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+                return View(viewModel);
+            }
             if (id != seller.Id)
             {
                 return BadRequest();
--- Modulo18/SistemaVendas/SistemaVendas/Model/Seller.cs
+++ Modulo18/SistemaVendas/SistemaVendas/Model/Seller.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 
 namespace SistemaVendas.Model
@@ -6,12 +7,28 @@ namespace SistemaVendas.Model
     public class Seller
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "{0} required")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage ="{0} size sould be between {2} and {1}")]
         public string Name { get; set; }
+        [DataType(DataType.EmailAddress)]
+        [EmailAddress(ErrorMessage ="Enter a valid email")]
         public string Email { get; set; }
+
+        [Display(Name = "Base Salary")]
+        [DisplayFormat(DataFormatString = "{0:F2}")]
+        [Required(ErrorMessage = "{0} required")]
         public double BaseSalary { get; set; }
+
+        [Display(Name = "Birth Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        [Required(ErrorMessage = "{0} required")]
         public DateTime BirthDate { get; set; }
 
         public Department Department { get; set; }
+
+        [Display(Name = "Department ID")]
+        [Required(ErrorMessage = "{0} required")]
         public int DepartmentId { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
 
